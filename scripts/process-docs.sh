1604b2faf5593bcb4c00ef57b5d3609d771b4a4c
YiniXu9506 <xuyini@pingcap.com>
2020-05-19 17:09:23 +0800

    cancel processing docs on preview branch

--- scripts/process-docs.sh
+++ scripts/process-docs.sh
@@ -25,77 +25,77 @@ replace_dist_html_link() {
   fi
 }
 
-cn_tmp_docs_path="dist/docs-cn"
-en_tmp_docs_path="dist/docs"
-tidb_docs_versions=(stable dev v2.1 v3.1 v3.0)
-operator_docs_versions=(stable dev v1.0)
-dm_docs_versions=(stable dev)
-misc_repos=(blog-cn blog meetup weekly)
+# cn_tmp_docs_path="dist/docs-cn"
+# en_tmp_docs_path="dist/docs"
+# tidb_docs_versions=(stable dev v2.1 v3.1 v3.0)
+# operator_docs_versions=(stable dev v1.0)
+# dm_docs_versions=(stable dev)
+# misc_repos=(blog-cn blog meetup weekly)
 
-python scripts/convert_html.py "$en_tmp_docs_path/index.html" "docs"
-python scripts/convert_html.py "$cn_tmp_docs_path/index.html" "docs-cn"
+# python scripts/convert_html.py "$en_tmp_docs_path/index.html" "docs"
+# python scripts/convert_html.py "$cn_tmp_docs_path/index.html" "docs-cn"
 
-start_time=$(date +%H%M%S)
-echo "start time" $start_time
-for v in "${tidb_docs_versions[@]}"
-do
-  echo "$en_tmp_docs_path/$v"
-  echo "$cn_tmp_docs_path/$v"
-  replace_dist_html_link "$en_tmp_docs_path/$v" "docs/$v" &
-  replace_dist_html_link "$cn_tmp_docs_path/$v" "docs-cn/$v" &
-done
+# start_time=$(date +%H%M%S)
+# echo "start time" $start_time
+# for v in "${tidb_docs_versions[@]}"
+# do
+#   echo "$en_tmp_docs_path/$v"
+#   echo "$cn_tmp_docs_path/$v"
+#   replace_dist_html_link "$en_tmp_docs_path/$v" "docs/$v" &
+#   replace_dist_html_link "$cn_tmp_docs_path/$v" "docs-cn/$v" &
+# done
 
-for v in "${operator_docs_versions[@]}"
-do 
-  echo "$en_tmp_docs_path/tidb-in-kubernetes/$v"
-  echo "$cn_tmp_docs_path/tidb-in-kubernetes/$v"
-  replace_dist_html_link "$en_tmp_docs_path/tidb-in-kubernetes/$v" "docs/tidb-in-kubernetes/$v" &
-  replace_dist_html_link "$cn_tmp_docs_path/tidb-in-kubernetes/$v" "docs-cn/tidb-in-kubernetes/$v" &
-done
+# for v in "${operator_docs_versions[@]}"
+# do 
+#   echo "$en_tmp_docs_path/tidb-in-kubernetes/$v"
+#   echo "$cn_tmp_docs_path/tidb-in-kubernetes/$v"
+#   replace_dist_html_link "$en_tmp_docs_path/tidb-in-kubernetes/$v" "docs/tidb-in-kubernetes/$v" &
+#   replace_dist_html_link "$cn_tmp_docs_path/tidb-in-kubernetes/$v" "docs-cn/tidb-in-kubernetes/$v" &
+# done
 
-for v in "${dm_docs_versions[@]}"
-do
-  echo "$en_tmp_docs_path/tidb-data-migration/$v"
-  echo "$cn_tmp_docs_path/tidb-data-migration/$v"
-  replace_dist_html_link "$en_tmp_docs_path/tidb-data-migration/$v" "docs/tidb-data-migration/$v" &
-  replace_dist_html_link "$cn_tmp_docs_path/tidb-data-migration/$v" "docs-cn/tidb-data-migration/$v" &
-done
+# for v in "${dm_docs_versions[@]}"
+# do
+#   echo "$en_tmp_docs_path/tidb-data-migration/$v"
+#   echo "$cn_tmp_docs_path/tidb-data-migration/$v"
+#   replace_dist_html_link "$en_tmp_docs_path/tidb-data-migration/$v" "docs/tidb-data-migration/$v" &
+#   replace_dist_html_link "$cn_tmp_docs_path/tidb-data-migration/$v" "docs-cn/tidb-data-migration/$v" &
+# done
 
-for repo in "${misc_repos[@]}"
-do 
-{
-  echo "dist/$repo"
-  replace_dist_html_link "dist/$repo" "$repo"
-} &
-done
+# for repo in "${misc_repos[@]}"
+# do 
+# {
+#   echo "dist/$repo"
+#   replace_dist_html_link "dist/$repo" "$repo"
+# } &
+# done
 
-# waiting all tasks finish.
-wait
+# # waiting all tasks finish.
+# wait
 
-end_time=$(date +%H%M%S)
-echo "end time" $end_time
-replace_dist_html_link "dist/cases-cn" blog-cn
-replace_dist_html_link "dist/success-stories" blog
-echo "finish all replacement"
+# end_time=$(date +%H%M%S)
+# echo "end time" $end_time
+# replace_dist_html_link "dist/cases-cn" blog-cn
+# replace_dist_html_link "dist/success-stories" blog
+# echo "finish all replacement"
 
-parent_dir="`echo $(pwd) | sed 's;/scripts;;g'`/dist"
-rm_images_from_media_docs() {
-  repo_name=$1
-  rm -rf $parent_dir/$repo_name/media
-}
+# parent_dir="`echo $(pwd) | sed 's;/scripts;;g'`/dist"
+# rm_images_from_media_docs() {
+#   repo_name=$1
+#   rm -rf $parent_dir/$repo_name/media
+# }
 
-# mv blog-cn/cases-cn in media to dist/images
-rm_images_from_media_docs docs/v2.1
-rm_images_from_media_docs docs/dev
-rm_images_from_media_docs docs/stable
-rm_images_from_media_docs docs/v3.1
-rm_images_from_media_docs docs/v3.0
-rm_images_from_media_docs docs-cn/v2.1
-rm_images_from_media_docs docs-cn/dev
-rm_images_from_media_docs docs-cn/stable
-rm_images_from_media_docs docs-cn/v3.1
-rm_images_from_media_docs docs-cn/v3.0
-rm_images_from_media_docs blog-cn
-rm_images_from_media_docs blog
-rm_images_from_media_docs weekly
-rm_images_from_media_docs meetup
+# # mv blog-cn/cases-cn in media to dist/images
+# rm_images_from_media_docs docs/v2.1
+# rm_images_from_media_docs docs/dev
+# rm_images_from_media_docs docs/stable
+# rm_images_from_media_docs docs/v3.1
+# rm_images_from_media_docs docs/v3.0
+# rm_images_from_media_docs docs-cn/v2.1
+# rm_images_from_media_docs docs-cn/dev
+# rm_images_from_media_docs docs-cn/stable
+# rm_images_from_media_docs docs-cn/v3.1
+# rm_images_from_media_docs docs-cn/v3.0
+# rm_images_from_media_docs blog-cn
+# rm_images_from_media_docs blog
+# rm_images_from_media_docs weekly
+# rm_images_from_media_docs meetup
